4274642cc3e4344ac2dadb411b7d5e49a92b4ed3
kyrie713 <519138523@qq.com>
2026-02-07 15:15:17 +0800

    last-commit-end-end

--- abstract-machine/klib/src/stdlib.c
+++ abstract-machine/klib/src/stdlib.c
@@ -28,22 +28,21 @@ int atoi(const char* nptr) {
   }
   return x;
 }
-static uintptr_t heap_cur = 0;//uintptr_t：无符号整型，能完整存放一个指针的数值
+static uintptr_t heap_cur = 0;
 void *malloc(size_t size) {
   // On native, malloc() will be called during initializaion of C runtime.
   // Therefore do not call panic() here, else it will yield a dead recursion:
   //   panic() -> putchar() -> (glibc) -> malloc() -> panic()
-  // 初始化时设置为堆区起始地址
   if(heap_cur == 0){
     heap_cur = (uintptr_t)heap.start;
   }
-  // 内存对齐到 8 字节加 7 是为了把“向下对齐”变成“向上对齐”
+
   size = (size + 7) & ~ 7;
-  // 检查是否超过堆区
+
   if (heap_cur + size > (uintptr_t)heap.end) {
-    return NULL;  // 堆满了，返回 NULL
+    return NULL;  
   }
-  // 返回当前地址，并更新分配指针
+
   void *ret = (void *)heap_cur;
   heap_cur += size;
   return ret;
